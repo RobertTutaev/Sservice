80e0e231b0c2b9341e05bfd3d9bf3cc75b6bc2c5
Пользователь <>
2017-12-06 17:20:28 +0500

    0.1.0

--- service/urls.py
+++ service/urls.py
@@ -3,11 +3,8 @@ from service import views
 
 urlpatterns = [
     url(r'^$', views.index, name='/'),
-    url(r'^about', views.about, name='/about'),
-    url(r'^contacts', views.contacts, name='/contacts'),
-    url(r'^profile$', views.profile, name='/profile'),
+    url(r'^about$', views.about, name='/about'),
+    url(r'^contacts$', views.contacts, name='/contacts'),
+    url(r'^profile', views.profile, name='/profile'),
+    url(r'^services$', views.services, name='/services'),
 ]
-
-
-
-
--- service/views.py
+++ service/views.py
@@ -3,13 +3,12 @@ from django.http.response import HttpResponse
 from django.template import Context
 from django.contrib.auth.decorators import login_required
 
-from service.models import SPage
+from service.models import SPage, SUserService, SService
 
 # Create your views here.
 
 def index(request):
     sPage = get_object_or_404(SPage, id=1)
-    print(sPage)
     return render(request, 'page.html', {'page': sPage})
 
 def about(request):
@@ -23,3 +22,15 @@ def contacts(request):
 @login_required
 def profile(request):
     return render(request, 'registration/profile.html')
+
+@login_required
+def services(request):    
+    #sUserServices = SUserService.objects.filter(user=request.user)
+    #services = SService.objects.filter(pk__in = sUserServices)
+
+    #sUserServices = SUserService.objects.get(user=request.user)
+    #services = sUserServices.sService.all()
+
+    services = SService.objects.filter(suserservice__user=request.user)
+    
+    return render(request, 'services.html', {'services': services})
