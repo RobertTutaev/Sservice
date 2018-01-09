54d5a6e1a58d01cd3e4497cc22b645bc511a1f0d
Пользователь <>
2018-01-09 17:12:12 +0500

    0.1.2

--- service/forms/main.py
+++ service/forms/main.py
@@ -1,5 +1,4 @@
 from django import forms
-from service.models import Article
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+class QueryForm(forms.Form):
+    snils = forms.CharField(label='СНИЛС', max_length=100)
